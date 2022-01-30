10df64f0cddfbf939fc8119964eb6b3c67a6eb6e
Niloo <niloofargst@gmail.com>
2022-01-30 18:04:14 +0100

    Updates

---
+++ precache-manifest.68ad58d26b957fec27b60f40d836ff98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0a3ce11468b99474646bbd4474b4057",
+    "url": "/project15/index.html"
+  },
+  {
+    "revision": "8f7664e34a972d284cec",
+    "url": "/project15/static/css/main.0e6e2d43.chunk.css"
+  },
+  {
+    "revision": "457868af8ec76675f7e9",
+    "url": "/project15/static/js/2.8d6a7d8a.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/project15/static/js/2.8d6a7d8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f7664e34a972d284cec",
+    "url": "/project15/static/js/main.8a1318d4.chunk.js"
+  },
+  {
+    "revision": "d46db7edc4e883f9d33e",
+    "url": "/project15/static/js/runtime-main.259fa77a.js"
+  }
+]);
